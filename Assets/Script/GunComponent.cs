623008c41d0d4ddbc563f4359fc1dd42b4351907
overcookedcircuit <112568917+overcookedcircuit@users.noreply.github.com>
2024-09-09 17:10:21 -0400

    lab 3 complete

---
+++ Assets/Script/GunComponent.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunComponent : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+    public float chargeSpeed = 10f;
+    private float chargeTime = 0f;
+    private float maxChargeTime = 5f;
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            chargeTime = 0f;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            chargeTime += Time.deltaTime;
+            chargeTime = Mathf.Clamp(chargeTime, 0, maxChargeTime);
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            // Spawn bullet when Fire1 is released
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            BulletComponent bulletComp = bullet.GetComponent<BulletComponent>();
+            bulletComp.bulletSpeed = chargeSpeed * chargeTime;
+        }
+    }
+}
---
+++ Assets/Script/TargetComponent.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetComponent : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            GameManager.Instance.IncrementScore();
+
+            // Hide target after 5 seconds
+            Invoke("HideTarget", 5f);
+
+
+            Renderer parentRenderer = transform.parent.GetComponent<Renderer>();
+            if (parentRenderer != null)
+            {
+                parentRenderer.material.color = Color.green;
+                Invoke("ChangeColorBack", 5f); // Change color back after 5 seconds
+            }
+        }
+    }
+
+    void HideTarget()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    void ChangeColorBack()
+    {
+        Renderer parentRenderer = transform.parent.GetComponent<Renderer>();
+        if (parentRenderer != null)
+        {
+            parentRenderer.material.color = Color.red;
+        }
+    }
+
+}
+
